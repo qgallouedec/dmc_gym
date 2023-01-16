53fa5c9116ad88edc2b9705b791fe8aea2a4fe35
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-01-16 17:15:20 +0100

    remove pygame dependency; Docstring DMCEnv, rm unused attr, rm useless action scaling

--- dmc_gym/dmc_env.py
+++ dmc_gym/dmc_env.py
@@ -5,11 +5,6 @@ from dm_control import suite
 from dm_env import TimeStep, specs
 from gym import Env, spaces
 
-try:
-    import pygame
-except:
-    pygame = None
-
 
 def extract_min_max(spec: Union[specs.Array, specs.BoundedArray]) -> Tuple[float, float]:
     """
@@ -30,7 +25,6 @@ def extract_min_max(spec: Union[specs.Array, specs.BoundedArray]) -> Tuple[float
         low, high = -np.inf, np.inf
 
     return (low * np.ones(dim)).astype(np.float32), (high * np.ones(dim)).astype(np.float32)
-
 
 
 def spec_to_box(specs: List[Union[specs.Array, specs.BoundedArray]]) -> spaces.Box:
@@ -69,6 +63,20 @@ def flatten_dict_observation(obs: Dict[str, Union[float, np.ndarray]]) -> np.nda
 
 
 class DMCEnv(Env):
+    """
+    Deepmind Control Suite Envrionment.
+
+    This class is a wrapper for integrating Deepmind Control Suite environment with gym.
+    See https://github.com/deepmind/dm_control.
+
+    Args:
+        domain_name (str): Domain name
+        task_name (str): Task name
+        from_pixels (bool, optional): If True, the observation is the
+            image from the scene. If False, it's the state as provided
+            by dm_control. Defaults to False.
+    """
+
     metadata = {"render.modes": ["rgb_array"]}
 
     def __init__(self, domain_name: str, task_name: str, from_pixels: bool = False) -> None:
@@ -78,8 +86,7 @@ class DMCEnv(Env):
         self._env = suite.load(domain_name=domain_name, task_name=task_name, task_kwargs={"random": [42]})
 
         # Create action space
-        self.inner_action_space = spec_to_box([self._env.action_spec()])
-        self.action_space = spaces.Box(low=-1.0, high=1.0, shape=self.inner_action_space.shape, dtype=np.float32)
+        self.action_space = spec_to_box([self._env.action_spec()])
 
         # Create observation space
         if from_pixels:
@@ -88,38 +95,23 @@ class DMCEnv(Env):
             self.observation_space = spec_to_box(self._env.observation_spec().values())
 
         self.reward_range = (0, 1)
-
-        # For humnan rendering
-        self.screen_size = None
-        self.window = None
-        self.clock = None
-        self.metadata["video.frames_per_second"] = int(1/self._env.physics.model.opt.timestep)
+        self.metadata["video.frames_per_second"] = int(1 / self._env.physics.model.opt.timestep)
 
     def __getattr__(self, name: str) -> Any:
         return getattr(self._env, name)
-
-    def _scale_action(self, action: np.ndarray) -> np.ndarray:
-        true_delta = self.inner_action_space.high - self.inner_action_space.low
-        norm_delta = self.action_space.high - self.action_space.low
-        action = (action - self.action_space.low) / norm_delta
-        action = action * true_delta + self.inner_action_space.low
-        return action
 
     def seed(self, seed: Optional[int]) -> None:
         self.action_space.seed(seed)
         self.observation_space.seed(seed)
 
     def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, Dict[str, Any]]:
-        action = self._scale_action(action)
-        reward = 0
         time_step = self._env.step(action)  # type: TimeStep
-        reward += time_step.reward
-        done = time_step.last()
         if self._from_pixels:
             obs = self._env.physics.render(height=84, width=84, camera_id=0)
         else:
             obs = flatten_dict_observation(time_step.observation).astype(np.float32)
-
+        reward = time_step.reward
+        done = time_step.last()
         info = {"internal_state": self._env.physics.get_state(), "discount": time_step.discount}
         return obs, reward, done, info
 
@@ -134,6 +126,5 @@ class DMCEnv(Env):
     def render(self, mode: int = "rgb_array") -> np.ndarray:
         if mode == "rgb_array":
             return self._env.physics.render(height=480, width=480, camera_id=0)
-        elif mode=="human":
+        elif mode == "human":
             raise NotImplementedError("Help welcomed.")
-
